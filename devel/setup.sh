d50fae396618ba729fc5b61fca0eea670ae2d1f7
Shobit-Sharma <shar0287@kettering.edu>
2017-09-08 20:13:15 -0400

    Fixed naming issue

--- devel/setup.sh
+++ devel/setup.sh
@@ -8,7 +8,7 @@
 
 # since this file is sourced either use the provided _CATKIN_SETUP_DIR
 # or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/mir-lab/Mini_Cooper/devel}
+: ${_CATKIN_SETUP_DIR:=/home/mir-lab/catkin_ws_mini_cooper/devel}
 _SETUP_UTIL="$_CATKIN_SETUP_DIR/_setup_util.py"
 unset _CATKIN_SETUP_DIR
 
