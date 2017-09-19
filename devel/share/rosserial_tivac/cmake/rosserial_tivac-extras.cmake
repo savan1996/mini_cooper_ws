f507d145eee9260e8e889d84ec735bf7aa302c1d
Shobit-Sharma <shar0287@kettering.edu>
2017-09-19 19:05:29 -0400

    Fixed naming issue

--- devel/share/rosserial_tivac/cmake/rosserial_tivac-extras.cmake
+++ devel/share/rosserial_tivac/cmake/rosserial_tivac-extras.cmake
@@ -1,3 +1,3 @@
 cmake_minimum_required(VERSION 2.8.3)
 
-set(ROSSERIAL_TIVAC_TOOLCHAIN "/home/mir-lab/catkin_ws_mini_cooper/src/include/rosserial_tivac/tivac-cmake/cmake/TivaCToolchain.cmake")
+set(ROSSERIAL_TIVAC_TOOLCHAIN "/home/ubuntu/catkin_ws_mini_cooper/src/include/rosserial_tivac/tivac-cmake/cmake/TivaCToolchain.cmake")
