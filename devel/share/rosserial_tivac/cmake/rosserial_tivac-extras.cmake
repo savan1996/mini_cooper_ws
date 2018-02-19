928fe98f30db149015b78dd9fc61c4361c62c643
Mohamed-Fasil <syed0216@kettering.edu>
2018-02-19 18:18:06 -0500

    removed rosserial

--- devel/share/rosserial_tivac/cmake/rosserial_tivac-extras.cmake
+++ devel/share/rosserial_tivac/cmake/rosserial_tivac-extras.cmake
@@ -1,3 +1,3 @@
 cmake_minimum_required(VERSION 2.8.3)
 
-set(ROSSERIAL_TIVAC_TOOLCHAIN "/home/ubuntu/catkin_ws_mini_cooper/src/include/rosserial_tivac/tivac-cmake/cmake/TivaCToolchain.cmake")
+set(ROSSERIAL_TIVAC_TOOLCHAIN "/home/mir-lab/mini_cooper_ws/src/rosserial/rosserial_tivac/tivac-cmake/cmake/TivaCToolchain.cmake")
--- install/share/rosserial_tivac/cmake/rosserial_tivacConfig-version.cmake
+++ install/share/rosserial_tivac/cmake/rosserial_tivacConfig-version.cmake
@@ -1,5 +1,5 @@
 # generated from catkin/cmake/template/pkgConfig-version.cmake.in
-set(PACKAGE_VERSION "0.7.5")
+set(PACKAGE_VERSION "0.7.7")
 
 set(PACKAGE_VERSION_EXACT False)
 set(PACKAGE_VERSION_COMPATIBLE False)
